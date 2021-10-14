24f8d1945a453eb643b40f47cfd1cf98fa870a74
Chris Krycho <hello@chriskrycho.com>
2021-10-14 10:37:57 -0600

    WIP

--- config/ember-try-typescript.js
+++ config/ember-try-typescript.js
@@ -16,6 +16,55 @@ module.exports = {
       },
     },
     {
+      command: 'tsc --noEmit --project type-tests/tsconfig-3.8.json',
+      name: 'typescript-3.8',
+      npm: {
+        typescript: '~3.8',
+      },
+    },
+    {
+      command: 'tsc --noEmit --project type-tests/tsconfig-3.9.json',
+      name: 'typescript-3.9',
+      npm: {
+        typescript: '~3.9',
+      },
+    },
+    {
+      command: 'tsc --noEmit --project type-tests/tsconfig-4.0.json',
+      name: 'typescript-4.0',
+      npm: {
+        typescript: '~4.0',
+      },
+    },
+    {
+      command: 'tsc --noEmit --project type-tests/tsconfig-4.1.json',
+      name: 'typescript-4.1',
+      npm: {
+        typescript: '~4.1',
+      },
+    },
+    {
+      command: 'tsc --noEmit --project type-tests/tsconfig-4.2.json',
+      name: 'typescript-4.2',
+      npm: {
+        typescript: '~4.2',
+      },
+    },
+    {
+      command: 'tsc --noEmit --project type-tests/tsconfig-4.3.json',
+      name: 'typescript-4.3',
+      npm: {
+        typescript: '~4.3',
+      },
+    },
+    {
+      command: 'tsc --noEmit --project type-tests/tsconfig-4.4.json',
+      name: 'typescript-4.4',
+      npm: {
+        typescript: '~4.4',
+      },
+    },
+    {
       command: 'tsc --noEmit --project type-tests/tsconfig-next.json',
       name: 'typescript-next',
       allowedToFail: true,
