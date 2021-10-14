3d9bc78ed076dacad3a3be7981e02ec4619635bd
Chris Krycho <hello@chriskrycho.com>
2021-10-14 10:43:36 -0600

    Drop support for old versions in Ember Try config
    - unsupported LTS versions of Ember
    - corresponding versions of TS

--- config/ember-try-typescript.js
+++ config/ember-try-typescript.js
@@ -2,48 +2,6 @@ module.exports = {
   useYarn: true,
   scenarios: [
     {
-      command: 'tsc --noEmit --project type-tests/tsconfig-3.6.json',
-      name: 'typescript-3.6',
-      npm: {
-        typescript: '~3.6',
-      },
-    },
-    {
-      command: 'tsc --noEmit --project type-tests/tsconfig-3.7.json',
-      name: 'typescript-3.7',
-      npm: {
-        typescript: '~3.7',
-      },
-    },
-    {
-      command: 'tsc --noEmit --project type-tests/tsconfig-3.8.json',
-      name: 'typescript-3.8',
-      npm: {
-        typescript: '~3.8',
-      },
-    },
-    {
-      command: 'tsc --noEmit --project type-tests/tsconfig-3.9.json',
-      name: 'typescript-3.9',
-      npm: {
-        typescript: '~3.9',
-      },
-    },
-    {
-      command: 'tsc --noEmit --project type-tests/tsconfig-4.0.json',
-      name: 'typescript-4.0',
-      npm: {
-        typescript: '~4.0',
-      },
-    },
-    {
-      command: 'tsc --noEmit --project type-tests/tsconfig-4.1.json',
-      name: 'typescript-4.1',
-      npm: {
-        typescript: '~4.1',
-      },
-    },
-    {
       command: 'tsc --noEmit --project type-tests/tsconfig-4.2.json',
       name: 'typescript-4.2',
       npm: {
